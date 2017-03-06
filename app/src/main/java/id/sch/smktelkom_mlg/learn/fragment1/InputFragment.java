4b92cc7949c366f1839cc212b34b3f3b6b4ccd3d
louisa <louisa_gabriella_24rpl@student.smktelkom-mlg.sch.id>
2017-03-06 10:09:31 +0700

    Fragment as View Only Reverse

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/InputFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/InputFragment.java
@@ -1,17 +1,20 @@
 package id.sch.smktelkom_mlg.learn.fragment1;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InputFragment extends Fragment {
+    EditText etNama;
+    EditText etUmur;
+
 
 
     public InputFragment() {
